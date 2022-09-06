1f4984c13d3255c0147e139fc5667653532ae606
Sadeepa <susinidusadeepa@gmail.com>
2022-09-06 22:03:19 +0530

    almost finish

--- js/Script.js
+++ js/Script.js
@@ -8,11 +8,9 @@ function myFunction(){
     
     
     var endTime =document.getElementById("endTime").value;
-    console.log (endTime);
     var numberOfLaps = document.getElementById("NoLaps").value;
 
     var Answer = (2*(22/7)*21)*numberOfLaps / (endTime - startTime);
 
-    alert(Answer);
-}
-    // document.getElementById("demo").innerHTML = "Speed Of Kamal" + "=" + Answer + "ms-1";
+     document.getElementById("demo").innerHTML = "Speed Of Kamal" + "=" + Answer + "ms-1";
+    }
