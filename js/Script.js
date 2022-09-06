e421840a7436f727c5f4dfa9aeec7e98c8a37b49
Sadeepa <susinidusadeepa@gmail.com>
2022-09-06 18:59:50 +0530

    add input to html add onsubmit function and add getinputvalue function

--- js/Script.js
+++ js/Script.js
@@ -1,9 +1,16 @@
-var startTime, endTime, numberOfLaps, circum, submit;
-startTime = 00;
-endTime =100;
-circum = (2* 22/7 * 21);
-numberOfLaps = circum * 2;
-submit = numberOfLaps / (endTime - startTime);
+function getInputValue() {
+var startTime, endTime, numberOfLaps, speed;
+var startTime =
+document.getElementById("stTime").value;
+
+var endTime =
+document.getElementById("endTime").value;
+
+var numberOfLaps =
+document.getElementById("NoLaps").value;
+var speed = numberOfLaps / (endTime - startTime);
+}
+
 
 function myfunction() {
     document.getElementById("demo").innerHTML = "Speed Of Kamal" + "=" + speed + "ms-1";
