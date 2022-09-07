21af8f60e58b65af12a74d67497b6a294d2f2d6c
Sadeepa <susinidusadeepa@gmail.com>
2022-09-07 15:10:27 +0530

    add myFunction and variables to js file

--- js/Script.js
+++ js/Script.js
@@ -13,5 +13,21 @@ function myFunction(){
 
 
 document.getElementById("demo").innerHTML = "Speed Of Kamal" + "=" + " " + Answer + "ms-1";
+}
     
+
+function myFunction(){
+    event.preventDefault()
+    var firstRunnerSpeed, secondRunnerSpeed, thirdRunnerSpeed, fourthRunnerSpeed, fifthRunnerSpeed, average;
+    var firstRunnerSpeed =document.getElementById("firstRSpeed").value;
+    var secondRunnerSpeed =document.getElementById("secondRSpeed").value;
+    var thirdRunnerSpeed =document.getElementById("thirdRSpeed").value;
+    var fourthRunnerSpeed =document.getElementById("fourthRSpeed").value;
+    var fifthRunnerSpeed =document.getElementById("fifthRSpeed").value;
+    
+
+    var speed =((firstRunnerSpeed)/5 + (secondRunnerSpeed)/5 + (thirdRunnerSpeed)/5 + (fourthRunnerSpeed)/5 + (fifthRunnerSpeed)/5);
+
+document.getElementById("Speed").innerHTML =speed + " " + "ms-1"; 
+
 }
